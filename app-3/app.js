f2054ca7b398e5106cf374ef2992def9c9e6391b
Alaa Alakkad <alaaalakkad@gmail.com>
2018-07-08 13:26:05 +0200

    comments added to database & seeding module

--- app-3/app.js
+++ app-3/app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const Campground = require("./models/campground");
+const seedDB = require("./seeds");
 const port = 3000;
 
 // setup
@@ -11,6 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+
+
 // database
 
 mongoose.connect('mongodb://localhost/yelp_camp').then(
@@ -18,33 +22,7 @@ mongoose.connect('mongodb://localhost/yelp_camp').then(
     (err)=>console.log(err)
 );
 
-const CampgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-const Campground = mongoose.model("Campground", CampgroundSchema);
-
-/* Campground.create({
-    name: "Hosea", 
-    image:"http://lablogbeaute.co.uk/wp-content/uploads/2016/07/camping.jpg",
-    description: "beautiful place"
-}, (err, campground)=>{
-    if(err) return console.log(err);
-    console.log("created successfully", campground);
-}) */
-
-/* let campgrounds = [
-    {name: "Hosea", image:"http://lablogbeaute.co.uk/wp-content/uploads/2016/07/camping.jpg"},
-    {name: "Dameon", image:"https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg"},
-    {name: "Maurine Hodkiewicz II", image:"https://www.nationalparks.nsw.gov.au/~/media/20E7E1F7667F42EEA33795B18189210E.ashx"},
-    {name: "Hosea", image:"http://lablogbeaute.co.uk/wp-content/uploads/2016/07/camping.jpg"},
-    {name: "Dameon", image:"https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg"},
-    {name: "Maurine Hodkiewicz II", image:"https://www.nationalparks.nsw.gov.au/~/media/20E7E1F7667F42EEA33795B18189210E.ashx"}
-
-]; */
-
+seedDB();
 
 // routes
 app.get("/", (req, res) =>{
@@ -90,8 +68,11 @@ app.get("/campgrounds/new", (req, res)=>{
 //SHOW
 app.get("/campgrounds/:id",(req, res)=>{
     // get the id and find query db
-    Campground.findById(req.params.id, (err, foundCamp)=>{
-        if(err) res.send("oops! something went wrong!");
+    Campground.findById(req.params.id).populate("comments").exec( (err, foundCamp)=>{
+        if(err) {
+            res.send("oops! something went wrong!");
+            return;
+        }
         res.render("show",{campground: foundCamp});
     })
 })
